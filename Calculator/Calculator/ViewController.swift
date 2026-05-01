d081a733184065ba53d0c829a38db66aad4f4422
Dalzoo <eastman36@naver.com>
2026-05-01 17:06:23 +0900

    Calculator V0 feat: stackVIew를 이용해서 전체적인 UI 구성 (storyboard는 삭제함) feat: 기본적인 연산 로직과 input 값에 따른 action 구현

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 황문주 on 5/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
