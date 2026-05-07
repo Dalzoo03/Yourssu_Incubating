5a566eae1d6e287b90f79822223c5962b3018689
Dalzoo <eastman36@naver.com>
2026-05-07 13:28:00 +0900

    🔨 [Refactor] 계산기 레이아웃 구현 및 로직 구조 개선
    UIStackView를 전체적으로 사용하고 세세한 Figma 레이아웃 수치 반영
    
    UIButton Extension 및 UITextField static 함수를 통한 UI 생성 로직 분리
    
    enum을 활용한 연산자 구분 및 연산 프로퍼티 사용해봄
    
    do-catch 문을 이용한 '0 나누기' 에러 처리

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,83 +7,73 @@
 
 import UIKit
 
+extension UIButton {
+    static func makeOperationButton(title: String) -> UIButton {
+        let btn = UIButton(type: .system)
+        btn.setTitle(title, for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 15)!
+        btn.backgroundColor = UIColor(red: 42/255, green: 161/255, blue: 187/255, alpha:1.0)
+        btn.layer.cornerRadius = 20
+        return btn
+    }
+}
+
 class ViewController: UIViewController {
     
-    let firstInputField: UITextField = {
-        let tf = UITextField()
-        tf.placeholder = "첫번째 숫자를 입력해주세요"
-        tf.borderStyle = .roundedRect
-        tf.keyboardType = .numberPad
-        tf.backgroundColor = UIColor(white: 0.94, alpha: 1.0)
-        tf.layer.cornerRadius = 10
-        tf.clipsToBounds = true
-        return tf
-    }()
+    private lazy var firstInputField: UITextField = makeInputField(inputText: "첫번째 숫자를 입력해주세요")
+    private lazy var secondInputField: UITextField = makeInputField(inputText: "두번째 숫자를 입력해주세요")
     
-    let secondInputField: UITextField = {
+    private func makeInputField (inputText:String) -> UITextField {
         let tf = UITextField()
-        tf.placeholder = "두번째 숫자를 입력해주세요"
+        tf.placeholder = inputText
         tf.borderStyle = .roundedRect
+        tf.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)!
         tf.keyboardType = .numberPad
-        tf.backgroundColor = UIColor(white: 0.94, alpha: 1.0)
+        tf.backgroundColor = .systemGray6
         tf.layer.cornerRadius = 10
         tf.clipsToBounds = true
         return tf
-    }()
+    }
     
     let resultLabel: UILabel = {
         let label = UILabel()
         label.text = "버튼을 눌러주세요"
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 16, weight: .light)
+        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 15)
         return label
     }()
     
-    let addButton: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("더하기", for: .normal)
-        btn.setTitleColor(.white, for: .normal)
-        btn.backgroundColor = UIColor(red: 45/255, green: 165/255, blue: 185/255, alpha: 1.0)
-        btn.layer.cornerRadius = 25
-        return btn
-    }()
+    let addButton = UIButton.makeOperationButton(title: "더하기")
+    let subButton = UIButton.makeOperationButton(title: "빼기")
+    let mulButton = UIButton.makeOperationButton(title: "곱하기")
+    let divButton = UIButton.makeOperationButton(title: "나누기")
     
-    let subButton: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("빼기", for: .normal)
-        btn.setTitleColor(.white, for: .normal)
-        btn.backgroundColor = UIColor(red: 45/255, green: 165/255, blue: 185/255, alpha: 1.0)
-        btn.layer.cornerRadius = 25
-        return btn
-    }()
-    
-    let mulButton: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("곱하기", for: .normal)
-        btn.setTitleColor(.white, for: .normal)
-        btn.backgroundColor = UIColor(red: 45/255, green: 165/255, blue: 185/255, alpha: 1.0)
-        btn.layer.cornerRadius = 25
-        return btn
-    }()
-    
-    let divButton: UIButton = {
-        let btn = UIButton(type: .system)
-        btn.setTitle("나누기", for: .normal)
-        btn.setTitleColor(.white, for: .normal)
-        btn.backgroundColor = UIColor(red: 45/255, green: 165/255, blue: 185/255, alpha: 1.0)
-        btn.layer.cornerRadius = 25
-        return btn
-    }()
-    
-    let mainStackView: UIStackView = {
+    private let inputStackView: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
-        stack.spacing = 15
-        stack.alignment = .fill
+        stack.spacing = 10
         stack.distribution = .fillEqually
+        return stack
+    }()
+
+    private let buttonStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 10
+        stack.distribution = .fillEqually
+        return stack
+    }()
+
+    private let mainStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fill
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -96,15 +86,29 @@ class ViewController: UIViewController {
     func setupUI() {
         view.addSubview(mainStackView)
         
-        [firstInputField, secondInputField, resultLabel, addButton, subButton, mulButton, divButton].forEach {
-            mainStackView.addArrangedSubview($0)
-            $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        inputStackView.addArrangedSubview(firstInputField)
+        inputStackView.addArrangedSubview(secondInputField)
+        
+        [addButton, subButton, mulButton, divButton].forEach {
+            buttonStackView.addArrangedSubview($0)
         }
         
+        mainStackView.addArrangedSubview(inputStackView)
+        mainStackView.addArrangedSubview(resultLabel)
+        mainStackView.addArrangedSubview(buttonStackView)
+        
+        [firstInputField, secondInputField].forEach {
+            $0.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        }
+            
+        [addButton, subButton, mulButton, divButton].forEach {
+            $0.heightAnchor.constraint(equalToConstant: 39).isActive = true // 버튼 높이
+        }
+            
         NSLayoutConstraint.activate([
-            mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 157),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 47),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -47)
         ])
     }
     
@@ -134,12 +138,28 @@ class ViewController: UIViewController {
         }
     }
     
-    @objc func addTapped() { calculate(operatorText: "+") }
-    @objc func subTapped() { calculate(operatorText: "-") }
-    @objc func mulTapped() { calculate(operatorText: "*") }
-    @objc func divTapped() { calculate(operatorText: "/") }
+    enum CalculatorOperator {
+        case add
+        case subtract
+        case multiply
+        case divide
+
+        var symbol: String {
+            switch self {
+            case .add: return "+"
+            case .subtract: return "-"
+            case .multiply: return "x"
+            case .divide: return "÷"
+            }
+        }
+    }
     
-    func calculate(operatorText: String) {
+    @objc func addTapped() { calculate(operatorText: .add) }
+    @objc func subTapped() { calculate(operatorText: .subtract) }
+    @objc func mulTapped() { calculate(operatorText: .multiply) }
+    @objc func divTapped() { calculate(operatorText: .divide) }
+    
+    func calculate(operatorText: CalculatorOperator) {
         let text1 = firstInputField.text ?? ""
         let text2 = secondInputField.text ?? ""
         
@@ -150,18 +170,29 @@ class ViewController: UIViewController {
         
         var result: Int = 0
         switch operatorText {
-        case "+": result = num1 + num2
-        case "-": result = num1 - num2
-        case "*": result = num1 * num2
-        case "/":
-            if num2 == 0 {
+        case .add: result = num1 + num2
+        case .subtract: result = num1 - num2
+        case .multiply: result = num1 * num2
+        case .divide:
+            do {
+                try result = safeDivide(num1, num2)
+            }
+            catch {
                 resultLabel.text = "0으로 나눌 수 없습니다"
                 return
             }
-            result = num1 / num2
-        default: break
         }
-        resultLabel.text = "\(num1) \(operatorText) \(num2) = \(result)"
+        resultLabel.text = "\(num1) \(operatorText.symbol) \(num2) = \(result)"
+    }
+    
+    enum CalculatingError: Error {
+        case divisionByZero
+    }
+    func safeDivide(_ operand1:Int, _ operand2:Int) throws  -> Int {
+        if operand2 == 0 {
+            throw CalculatingError.divisionByZero
+        }
+        return operand1 / operand2
     }
 }
 
